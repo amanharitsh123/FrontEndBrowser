e1ffe44ab87a387d314bdba7d922a272b2c9eb2f
amanharitsh123 <amanharitsh123@gmail.com>
2018-02-11 00:53:04 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import gi
+gi.require_version('WebKit2', '4.0')
+from gi.repository import WebKit2
+from gi.repository import Gtk
+
+def close(window):
+        Gtk.main_quit()
+
+def main():
+        Gtk.init()
+        
+        view = WebKit2.WebView()
+        view.load_uri("http://localhost:8080/")
+        window = Gtk.Window()
+        window.add(view)
+        window.set_decorated(False)
+        window.maximize()
+        window.show_all()
+        Gtk.main()
+
+main()
