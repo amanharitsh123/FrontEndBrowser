df27513910689263c81b030da8fa8a4d93174a03
Aman Sharma <amanharitsh123@gmail.com>
2018-02-11 00:54:31 +0530

    Removed extra Close Function

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ gi.require_version('WebKit2', '4.0')
 from gi.repository import WebKit2
 from gi.repository import Gtk
 
-def close(window):
-        Gtk.main_quit()
-
 def main():
         Gtk.init()
         
